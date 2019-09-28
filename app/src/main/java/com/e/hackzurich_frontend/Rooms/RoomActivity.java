ed26601e7cc06d60d94acd8e64f5570c62c2fe86
tinorellstab <tino.rellstab@gmail.com>
2019-09-28 19:57:48 +0200

    layout create room

--- app/src/main/java/com/e/hackzurich_frontend/Rooms/RoomActivity.java
+++ app/src/main/java/com/e/hackzurich_frontend/Rooms/RoomActivity.java
@@ -4,19 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+
 
 import com.e.hackzurich_frontend.R;
 
 public class RoomActivity extends AppCompatActivity {
 
+    String roomName = "Coffee Meet Up";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_room);
+        this.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title);
 
-        Intent intent = getIntent();
-        String id = intent.getStringExtra("id");
+        setTitle(roomName);
 
-        Log.d("TAG", "onCreate: " + id.toString());
+
+        //Intent intent = getIntent();
+        //String id = intent.getStringExtra("id");
+
+        //Log.d("TAG", "onCreate: " + id.toString());
     }
 }
