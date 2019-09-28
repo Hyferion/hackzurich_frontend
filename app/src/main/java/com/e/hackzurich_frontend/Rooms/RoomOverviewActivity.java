3778ac0a025eb466ebfdcd0ee1d4f6379443384f
Silas Stulz <siou10@gmail.com>
2019-09-28 23:23:46 +0200

    location fix

--- app/src/main/java/com/e/hackzurich_frontend/Rooms/RoomOverviewActivity.java
+++ app/src/main/java/com/e/hackzurich_frontend/Rooms/RoomOverviewActivity.java
@@ -1,14 +1,21 @@
 package com.e.hackzurich_frontend.Rooms;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -86,7 +93,7 @@ public class RoomOverviewActivity extends AppCompatActivity {
             }
         });
 
-        getRooms();
+       // getRooms();
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(false);
@@ -99,10 +106,38 @@ public class RoomOverviewActivity extends AppCompatActivity {
 
         getUserInstance();
 
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                //Location Permission already granted
+                requestLocation();
+            } else {
+                //Request Location Permission
+                checkLocationPermission();
+                requestLocation();
+            }
+        } else {
+            requestLocation();
+        }
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        requestLocation();
+        rooms.clear();
+        getRooms();
+    }
+
+    public void requestLocation() {
         fusedLocationProviderClient.getLastLocation()
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
+                        Log.d("LOCATION", "onSuccess: " + Double.toString(location.getLongitude()));
                         updatePosition(location);
                         if (location != null) {
                             // Logic to handle location object
@@ -110,10 +145,7 @@ public class RoomOverviewActivity extends AppCompatActivity {
                     }
                 });
 
-
     }
-
-
 
 
     public void checkIfNameIsSetAndRedirect() {
@@ -206,6 +238,44 @@ public class RoomOverviewActivity extends AppCompatActivity {
         };
         queue.add(jsonObjectRequest);
 
+    }
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Location Permission Needed")
+                        .setMessage("This app needs the Location permission, please accept to use location functionality")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(RoomOverviewActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+        }
     }
 
 
