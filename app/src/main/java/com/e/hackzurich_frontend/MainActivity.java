a5abd961c4349fd758d0e19f8eec39c0fcff50e8
tinorellstab <tino.rellstab@gmail.com>
2019-09-28 11:13:02 +0200

    join name activity

--- app/src/main/java/com/e/hackzurich_frontend/MainActivity.java
+++ app/src/main/java/com/e/hackzurich_frontend/MainActivity.java
@@ -1,14 +1,17 @@
 package com.e.hackzurich_frontend;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     Button startBtn;
+    private SharedPreferences myPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         startBtn = (Button) findViewById(R.id.button);
-
         startBtn.setOnClickListener(startListener);
     }
 
@@ -26,9 +28,26 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             // TODO: Check if already logged in & Check if already has an user id
-            Intent intent = new Intent(MainActivity.this, NameActivity.class);
+
+
+            SharedPreferences myPreferences = getSharedPreferences("mypreference",
+                    Context.MODE_PRIVATE);
+
+            //myPreferences.edit().clear().commit();
+
+
+            if(myPreferences.contains("Name")){
+                // start Room activity
+                Intent intent = new Intent(MainActivity.this, RoomActivity.class);
+                startActivity(intent);
+            }else{
+                // start Name activity
+                Intent intent = new Intent(MainActivity.this, NameActivity.class);
+                startActivity(intent);
+            }
         }
     };
+
 
 
 
--- app/src/main/java/com/e/hackzurich_frontend/NameActivity.java
+++ app/src/main/java/com/e/hackzurich_frontend/NameActivity.java
@@ -1,13 +1,64 @@
 package com.e.hackzurich_frontend;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class NameActivity extends AppCompatActivity {
+    public SharedPreferences myPreferences;
+
+
+    EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
+
+        final SharedPreferences myPreferences = getSharedPreferences("mypreference",
+                Context.MODE_PRIVATE);
+
+        final EditText editText = (EditText) findViewById(R.id.editUsername);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+
+
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+
+                    SharedPreferences.Editor ed = myPreferences.edit();
+                    ed.putString("Name", editText.getText().toString());
+                    ed.commit();
+
+                    // send username, get user id
+
+                    Intent intent = new Intent(NameActivity.this, RoomActivity.class);
+                    startActivity(intent);
+
+
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+
+
     }
 }
