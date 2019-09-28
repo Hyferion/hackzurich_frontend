324714630ec2d6901dfad5f7850eeb240604e374
tinorellstab <tino.rellstab@gmail.com>
2019-09-28 20:35:16 +0200

    activity

--- app/src/main/java/com/e/hackzurich_frontend/Rooms/RoomActivity.java
+++ app/src/main/java/com/e/hackzurich_frontend/Rooms/RoomActivity.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.e.hackzurich_frontend.MainActivity;
+import com.e.hackzurich_frontend.NameActivity;
 import com.e.hackzurich_frontend.R;
 import com.google.gson.Gson;
 
@@ -20,6 +24,8 @@ import org.json.JSONObject;
 
 public class RoomActivity extends AppCompatActivity {
 
+    Button shareBtn;
+
     private String id;
     private RequestQueue queue;
 
@@ -28,9 +34,12 @@ public class RoomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_room);
-        this.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title);
 
-        setTitle(roomName);
+
+
+
+        shareBtn = (Button) findViewById(R.id.button6);
+        shareBtn.setOnClickListener(startListener);
 
         queue = Volley.newRequestQueue(this);
 
@@ -41,6 +50,17 @@ public class RoomActivity extends AppCompatActivity {
 
         calculate();
     }
+
+    private View.OnClickListener startListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v){
+            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Room Id");
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, id);
+            startActivity(Intent.createChooser(sharingIntent, "Share via"));
+        }
+    };
 
 
     public void calculate() {
